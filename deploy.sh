cc23cc1a8983c9db7342d66f49674f8cbf8eead3
Andrew Rea <email@andrewrea.co.uk>
2016-09-22 03:58:26 +0000

    INIT: initial commit on gh-pages branch

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,40 @@
-#!/bin/bash
+# Create a new orphand branch (no commit history) named gh-pages
+git checkout --orphan gh-pages
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# Unstage all files
+git rm --cached $(git ls-files)
 
-# Build the project.
-hugo
+# Grab one file from the master branch so we can make a commit
+git checkout master README.md
 
-# Add changes to git.
+# Add and commit that file
+git add .
+git commit -m "INIT: initial commit on gh-pages branch"
+
+# Push to remote gh-pages branch
+git push origin gh-pages
+
+# Return to master branch
+git checkout master
+
+# Remove the public folder to make room for the gh-pages subtree
+rm -rf public
+
+# Add the gh-pages branch of the repository. It will look like a folder named public
+git subtree add --prefix=public git@github.com:guzzlerio/docs.corcel.io.git gh-pages --squash
+
+# Pull down the file we just committed. This helps avoid merge conflicts
+git subtree pull --prefix=public git@github.com:guzzlerio/docs.corcel.io.git gh-pages
+
+# Run hugo. Generated site will be placed in public directory (or omit -t ThemeName if you're not using a theme)
+hugo -t corcel-hugo
+
+
+# Add everything
 git add -A
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-then msg="$1"
-fi
-git commit -m "$msg"
+# Commit and push to master
+git commit -m "Updating site" && git push origin master
 
-# Push source and build repos.
-git push origin master
-git subtree pull --prefix=public git@github.com:guzzlerio/docs.corcel.io.git gh-pages
+# Push the public subtree to the gh-pages branch
 git subtree push --prefix=public git@github.com:guzzlerio/docs.corcel.io.git gh-pages
