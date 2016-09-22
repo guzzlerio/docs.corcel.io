93526f53594d90283e210ff03564090b729e9927
Andrew Rea <email@andrewrea.co.uk>
2016-09-22 04:08:57 +0000

    INIT: initial commit on gh-pages branch

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+#echo "DON'T USE THIS ANYMORE - LEAVING IT TO SHOW THE HEAD F**K THAT GH-PAGES ADDING A CNAME CAUSED!! ARRGGGH"
+#exit
+
 # Create a new orphand branch (no commit history) named gh-pages
 git checkout --orphan gh-pages
 
@@ -21,10 +24,10 @@ git checkout master
 rm -rf public
 
 # Add the gh-pages branch of the repository. It will look like a folder named public
-git subtree add --prefix=public git@github.com:spencerlyon2/hugo_gh_blog.git gh-pages --squash
+git subtree add --prefix=public git@github.com:guzzlerio/docs.corcel.io.git gh-pages --squash
 
 # Pull down the file we just committed. This helps avoid merge conflicts
-git subtree pull --prefix=public git@github.com:spencerlyon2/hugo_gh_blog.git gh-pages
+git subtree pull --prefix=public git@github.com:guzzlerio/docs.corcel.io.git gh-pages
 
 # Run hugo. Generated site will be placed in public directory (or omit -t ThemeName if you're not using a theme)
 hugo -t hugo-corcel
@@ -39,4 +42,4 @@ git add -A
 git commit -m "Updating site" && git push origin master
 
 # Push the public subtree to the gh-pages branch
-git subtree push --prefix=public git@github.com:spencerlyon2/hugo_gh_blog.git gh-pages
+git subtree push --prefix=public git@github.com:guzzlerio/docs.corcel.io.git gh-pages
