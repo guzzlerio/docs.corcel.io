605f565609b76a71d44b86df34dfab82a15a904e
Andrew Rea <email@andrewrea.co.uk>
2016-11-19 17:33:59 +0000

    rebuilding site Sat Nov 19 17:33:59 UTC 2016

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-(mkdir -p themes && \
-    git clone git@github.com:guzzlerio/theme.corcel.io.git themes/theme.corcel.io &> /dev/null && \
-    cd themes/theme.corcel.io && \
-    git pull)
-
 curl -o tags.json https://api.github.com/repos/guzzlerio/deride
 
 # Build the project.
